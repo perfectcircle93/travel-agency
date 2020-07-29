e87bb6124cd1aa1ce596a0a6ec79a1e63239d498
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-29 11:58:52 +0200

    Add Trip and Country components views

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 
 import Home from './components/views/Home/Home';
 import Trips from './components/views/Trips/TripsContainer';
+// TODO - import other views
 import Countries from './components/views/Countries/CountriesContainer';
 import Regions from './components/views/Regions/RegionsContainer';
+import Country from './components/views/Country/CountryContainer';
+import Trip from './components/views/Trip/TripContainer';
 import Info from './components/views/Info/Info';
 import NotFound from './components/views/NotFound/NotFound';
 
@@ -43,6 +46,8 @@ class App extends React.Component {
             <Route exact path='/trips' component={Trips} />
             <Route exact path='/countries' component={Countries} />
             <Route exact path='/regions' component={Regions} />
+            <Route exact path='/country/:id' component={Country} />
+            <Route exact path='/trip/:id' component={Trip} />
             <Route exact path='/info' component={Info} />
             <Route path='*' component={NotFound} />
           </Switch>
