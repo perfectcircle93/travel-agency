c2274342e35030958b1245c6043f2eee0dbda9ed
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-31 10:59:58 +0200

    Add OrderForm component

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 // import styles from './OrderForm.scss';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
+import OrderOption from '../OrderOption/OrderOption';
+import pricing from '../../../data/pricing.json';
 
 
 class OrderForm extends React.Component {
@@ -10,6 +12,11 @@ class OrderForm extends React.Component {
     const {tripCost, options} = this.props;
     return (
       <Row>
+        {pricing.map(option => (
+          <Col md={4} key={option.id}>
+            <OrderOption {...option} />
+          </Col>
+        ))}
         <Col xs={12}>
           <OrderSummary tripCost={tripCost} options={options}/>
         </Col>
