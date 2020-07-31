fc9b587ef3f83b0db3e51b68768ceef3e472cf7a
Magdalena Świderska <m.swiderska@outlook.com>
2020-07-31 10:32:28 +0200

    Add utils files&OrderForm&OrderSummary

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styles from './OrderForm.scss';
+import {Row, Col} from 'react-flexbox-grid';
+import OrderSummary from '../OrderSummary/OrderSummary';
+
+
+class OrderForm extends React.Component {
+  render () {
+    const {tripCost, options} = this.props;
+    return (
+      <Row>
+        <Col xs={12}>
+          <OrderSummary tripCost={tripCost} options={options}/>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
+
+export default OrderForm;
