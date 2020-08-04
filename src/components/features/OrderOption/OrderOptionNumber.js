683b9f0e14887215fb92521166d605043b75173e
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-04 10:41:38 +0200

    Add OrderOptionNumber component

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { formatPrice } from '../../../utils/formatPrice';
+import styles from './OrderOption.scss';
+//import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
-const OrderOptionNumber = () => (
-  <div>
-    OrderOptionNumber
+
+const OrderOptionNumber = ({currentValue, limits, setOptionValue, price}) => (
+  <div className={styles.number}>
+    <input
+      type='number'
+      className={styles.inputSmall}
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+    />
+    ({formatPrice(price)})
+
   </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.number,
+  setOptionValue: PropTypes.func,
+  limits: PropTypes.object,
+  price: PropTypes.string,
+
+};
 
 export default OrderOptionNumber;
