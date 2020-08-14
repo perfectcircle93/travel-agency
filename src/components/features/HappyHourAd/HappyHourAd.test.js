a1a4f04b141c34fc731009cd89f96aeb307591d5
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-14 12:31:25 +0200

    Add test 2 render two elements

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -2,10 +2,22 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import HappyHourAd from './HappyHourAd';
 
+const select = {
+  title: '.title',
+  promoDescription: '.promoDescription',
+};
+
 describe('Component HappyHourAd', () => {
   it('should render without crashing', () => {
     const component = shallow(<HappyHourAd />);
     expect(component).toBeTruthy();
+  });
+
+  it('should render heading and description', () => {
+    const component = shallow (<HappyHourAd />);
+    // console.log(component.debug());
+    expect(component.exists(select.title)).toEqual(true);
+    expect(component.exists(select.promoDescription)).toEqual(true);
   });
 
 });
