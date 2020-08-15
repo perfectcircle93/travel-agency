d01307f18eceae345086197d3954b4ffda4a6b5a
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-15 15:51:43 +0200

    Add test 5 seconds delay

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -3,6 +3,13 @@ import React from 'react';
 //import PropTypes from 'prop-types';
 
 class HappyHourAd extends React.Component {
+  constructor(){
+    super();
+  
+    /* run this.forceUpdate() every second */
+    setInterval(() => this.forceUpdate(), 1000);
+  }
+
   getCountdownTime(){
     const currentTime = new Date();
     const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
