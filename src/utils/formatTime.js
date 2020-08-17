bf21a85cfa8871c7ea96b91a8de89e498a208d21
Magdalena Świderska <m.swiderska@outlook.com>
2020-08-17 09:26:25 +0200

    Add test 4&5 if ok will return hh:mm:ss

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -10,4 +10,21 @@ export const formatTime = (countDown) => {
   else if (countDown < 0) {
     return null;
   }
+
+  else {
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+
+    seconds = (seconds + Math.floor(countDown % 60) + '').padStart(2, '0');
+    console.log(seconds);
+
+    minutes = (minutes + Math.floor((countDown / 60) % 60) + '').padStart(2, '0');
+    console.log(minutes);
+
+    hours = (hours + Math.floor(countDown / 3600) + '').padStart(2, '0');
+    console.log(hours);
+
+    return hours + ':' + minutes + ':' + seconds;
+  }
 };
